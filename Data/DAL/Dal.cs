ec6f7bf3b4d08328ae9c934cebf3fd99ebdb5cf4
servan kılınç <121825907+servankilinc1@users.noreply.github.com>
2023-11-25 23:39:50 +0300

    first

---
+++ Data/DAL/Dal.cs
@@ -0,0 +1,21 @@
+﻿using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DAL
+{
+    public class Dal : IDal
+    {
+        public Value GetValue()
+        {
+            return new Value()
+            {
+                Id = 191,
+                Name = "Temp Valuye Name"
+            };
+        }
+    }
+}
---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Data.DAL;
+using Data.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IDal _dal;
+        public ValuesController(IDal dal)
+        {
+            _dal = dal;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(_dal.GetValue());
+        }
+    }
+}
